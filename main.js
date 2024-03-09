f37f165868c7f5a147422d214ade1ef80054b678
Nousheen Atif <onlinework42101@gmail.com>
2024-03-09 15:40:11 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,112 @@
+// OPERATORS
+// ARITHMETIC OPERATORS
+// ADDITIONS
+// EXAMPLES
+let number = 1;
+let numbers = 2;
+console.log(number + numbers);
+// ANS = 3
+let str1 = "1";
+let str2 = "2";
+console.log(str1 + str2);
+// ANS = 12
+let num1 = 2;
+let num2 = 3;
+let cart = num1 + num2;
+console.log(cart);
+// ANS = 5
+let numOfFaculty = 4;
+let numOfManagement = 3;
+console.log(numOfFaculty + numOfManagement);
+// ANS  = 7
+// COMBINING OPERATORS
+// EXAMPLE
+let numOfFaculty1 = 4;
+let numOfManagement1 = 3;
+let numOfAbsentFaculty = 2;
+console.log(numOfFaculty1 + numOfManagement1 - numOfAbsentFaculty);
+// ANS = 5 
+let numOfFacullty = 4;
+let numOfManagementt = 3;
+let numOfAbsentFacultyy = 2;
+let numOfTotalFaculty = numOfFacullty * 21;
+console.log(numOfTotalFaculty);
+// ANS = 84
+let numOfFaaculty = 4;
+let numOfMaanagement = 3;
+let numOfAbsentFaaculty = 2;
+let numOfTotalFaculty1 = numOfFaaculty * 21;
+let totalNumOfKg = 20;
+console.log(totalNumOfKg / numOfTotalFaculty1);
+let result = 3 + 4 * 5;
+console.log(result);
+// ANS = 23
+let result1 = 3 + 4 * 5;
+result1++;
+console.log(result1);
+// //  ANS = 24
+//   ARITHMETIC OPERATORS
+//   SUBBTRACTION
+//   EXAMPLES
+let tree = 5;
+let trees = 2;
+console.log(tree - trees);
+//  ANS = 3
+let flower = 1;
+let folwers = 2;
+console.log(flower - folwers);
+// ANS = -1
+let cart1 = 5;
+let num3 = 2;
+let total = cart1 - num3;
+console.log(total);
+//  ANS = 3
+//  ARITHMETIC OPERATORS
+//   MULTIPLICATION
+//   EXAMPLES
+let bags = 4;
+let apples = 5;
+let totalApples = bags * apples;
+console.log(totalApples);
+// //  ANS = 20
+let chalk = 5;
+let chalks = 2;
+console.log(chalk * chalks);
+// // ANS = 10
+// ARITHMETIC OPERATORS
+//   DIVISION
+//   EXAMPLES
+let table = 4;
+let tables = 2;
+console.log(table / tables);
+// // // ANS = 2
+let totalApples1 = 20;
+let bags1 = 4;
+let eachbags = totalApples1 / bags1;
+console.log(eachbags);
+// //  ANS = 5
+// ARITHMETIC OPERATORS
+// EXPONENTIATION
+//   EXAMPLES
+let orange = 2;
+let oranges = 2;
+console.log(orange ** oranges);
+// // // // ANS = 4
+let layer = 5;
+let mango = 5;
+let power = layer ** 2;
+console.log(power);
+// // // //  ANS = 25
+// // ARITHMETIC OPERATORS
+// // MODULUS
+// // EXAMPLES
+let pencil = 10;
+let pencils = 3;
+console.log(pencil % pencils);
+// // // ANS = 1
+let apples1 = 5;
+let bags2 = 2;
+let reminder = bags2 % apples1;
+console.log(reminder);
+export {};
+// // // //  ANS = 2  
---
+++ main.ts
@@ -0,0 +1,130 @@
+import { AsyncLocalStorage } from "async_hooks";
+
+// OPERATORS
+// ARITHMETIC OPERATORS
+// ADDITIONS
+// EXAMPLES
+
+let number = 1;
+let numbers = 2;
+console.log(number + numbers);
+// ANS = 3
+let str1 = "1";
+let str2 = "2";
+console.log(str1 + str2);
+// ANS = 12
+let num1: number = 2;
+let num2: number = 3;
+let cart: number = num1 + num2;
+console.log(cart);
+// ANS = 5
+let numOfFaculty = 4;
+let numOfManagement = 3;
+console.log(numOfFaculty + numOfManagement);
+// ANS  = 7
+
+// COMBINING OPERATORS
+// EXAMPLE
+let numOfFaculty1 = 4;
+let numOfManagement1 = 3;
+let numOfAbsentFaculty = 2;
+console.log(numOfFaculty1 + numOfManagement1 - numOfAbsentFaculty);
+// ANS = 5 
+
+let numOfFacullty = 4;
+let numOfManagementt = 3;
+let numOfAbsentFacultyy = 2;
+let numOfTotalFaculty = numOfFacullty *21;
+console.log(numOfTotalFaculty);
+// ANS = 84
+
+let numOfFaaculty = 4;
+let numOfMaanagement = 3;
+let numOfAbsentFaaculty = 2;
+let numOfTotalFaculty1 = numOfFaaculty *21;
+let totalNumOfKg = 20;
+console.log(totalNumOfKg /numOfTotalFaculty1 );
+
+let result = 3 + 4 * 5;
+console.log(result);
+// ANS = 23
+
+let result1 = 3 + 4 * 5;
+result1++;
+console.log(result1);
+// //  ANS = 24
+
+//   ARITHMETIC OPERATORS
+//   SUBBTRACTION
+//   EXAMPLES
+
+ let tree = 5;
+ let trees = 2;
+ console.log(tree - trees);
+  //  ANS = 3
+ let flower = 1;
+ let folwers = 2;
+ console.log(flower - folwers);
+ // ANS = -1
+let cart1 : number = 5;
+let num3 : number = 2;
+let total : number = cart1 - num3 ;
+console.log(total);
+//  ANS = 3
+
+//  ARITHMETIC OPERATORS
+//   MULTIPLICATION
+//   EXAMPLES
+
+let bags: number = 4;
+let apples : number = 5;
+let totalApples : number = bags * apples ;
+console.log(totalApples);
+// //  ANS = 20
+let chalk = 5;
+let chalks = 2;
+console.log( chalk * chalks);
+// // ANS = 10
+
+// ARITHMETIC OPERATORS
+//   DIVISION
+//   EXAMPLES
+
+ let table = 4;
+ let tables = 2;
+ console.log( table / tables);
+// // // ANS = 2
+let totalApples1: number = 20;
+let bags1 : number = 4;
+let eachbags : number = totalApples1 / bags1;
+console.log(eachbags);
+// //  ANS = 5
+
+// ARITHMETIC OPERATORS
+// EXPONENTIATION
+//   EXAMPLES
+
+ let orange = 2;
+ let oranges = 2;
+ console.log(orange ** oranges);
+// // // // ANS = 4
+
+let layer: number = 5;
+let mango : number = 5;
+let power : number = layer ** 2 ;
+console.log(power);
+// // // //  ANS = 25
+
+// // ARITHMETIC OPERATORS
+// // MODULUS
+// // EXAMPLES
+
+let pencil = 10;
+let pencils = 3;
+console.log(pencil % pencils);
+// // // ANS = 1
+let apples1 = 5;
+let bags2 = 2;
+let reminder = bags2 % apples1 ;
+console.log(reminder);
+// // // //  ANS = 2  
